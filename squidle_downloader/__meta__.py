480259a64dc69e7c157bd72147d64a23cf02edf9
Scott Lowe <scott.code.lowe@gmail.com>
2021-04-06 17:37:13 +0100

    Initialise project from template repository

---
+++ squidle_downloader/__meta__.py
@@ -0,0 +1,8 @@
+name = "squidle-downloader"
+path = name.lower().replace("-", "_").replace(" ", "_")
+version = "0.1.dev0"
+author = "Scott Lowe"
+author_email = "scott.code.lowe@gmail.com"
+description = ""
+url = "https://github.com/DalhousieAI/squidle-downloader"
+license = "closed"
