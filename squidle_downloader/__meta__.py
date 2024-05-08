d301e357a6f42c072554778f6bcb3b279d186763
Scott Lowe <scott.code.lowe@gmail.com>
2024-05-09 00:40:06 +0100

    REL: Version 1.0.0

--- squidle_downloader/__meta__.py
+++ squidle_downloader/__meta__.py
@@ -1,6 +1,6 @@
 name = "squidle-downloader"
 path = name.lower().replace("-", "_").replace(" ", "_")
-version = "0.1.dev0"
+version = "1.0.0"
 author = "Scott Lowe"
 author_email = "scott.code.lowe@gmail.com"
 description = ""
